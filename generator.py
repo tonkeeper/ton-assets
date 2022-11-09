c2a9e97b754b93304cfd845b4ec24a4d5cfdf38e
Denis Subbotin <denis@subbot.in>
2022-11-09 14:10:55 +0300

    normalize accounts

--- generator.py
+++ generator.py
@@ -2,6 +2,7 @@
 import json
 import yaml
 import glob
+import base64
 
 EXPLORER_JETTONS = "https://tonapi.io/jetton/"
 EXPLORER_ACCOUNTS = "https://tonapi.io/account/"
@@ -24,6 +25,8 @@ def merge_accounts(accounts):
     for file in ('accounts/givers.yaml', 'accounts/custodians.yaml', 'accounts/bridges.yaml', 'accounts/validators.yaml'):
         accounts.extend(yaml.safe_load(open(file)))
     with open('accounts.json', 'w') as out:
+        for a in accounts:
+            a['address'] = normalize_address(a['address'], True)
         json.dump(accounts, out, indent=" ", sort_keys=True)
     return main_page
 
@@ -38,12 +41,23 @@ def merge_collections():
 def main():
     jettons = merge_jettons()
     collections = merge_collections()
-    accounts = merge_accounts([{"name":x[0], "address": x[1]} for x in jettons+collections])
-    jettons_md = "\n".join(["[%s](%s%s) | %s" %(j[0],EXPLORER_JETTONS, j[1], j[1]) for j in jettons])
-    accounts_md = "\n".join(["[%s](%s%s) | %s" %(j[0], EXPLORER_ACCOUNTS, j[1], j[1]) for j in accounts])
+    accounts = merge_accounts([{"name": x[0], "address": x[1]} for x in jettons+collections])
+    jettons_md = "\n".join(["[%s](%s%s) | %s" % (j[0], EXPLORER_JETTONS, j[1], j[1]) for j in jettons])
+    accounts_md = "\n".join(["[%s](%s%s) | %s" % (j[0], EXPLORER_ACCOUNTS, j[1], j[1]) for j in accounts])
     collections_md = "\n".join(["[%s](%s%s) | %s" % (j[0], EXPLORER_COLLECTIONS, j[1], j[1]) for j in collections])
 
-    open('README.md', 'w').write(open("readme.md.template").read() % (accounts_md, collections_md, jettons_md) )
+    open('README.md', 'w').write(open("readme.md.template").read() % (accounts_md, collections_md, jettons_md))
+
+
+def normalize_address(a, to_raw):
+    if len(a) == 48:
+        raw = base64.urlsafe_b64decode(a)
+        workchain = raw[1]
+        if workchain == 255:
+            workchain = -1
+        addr = raw[2:34]
+        return "%d:%s" % (workchain, addr.hex())
+    return a.lower()
 
 
 if __name__ == '__main__':
