13307336a2c2243a2bc4aff2af828b49f8268f21
vodkashot <183611920+Vodkashot28@users.noreply.github.com>
2025-02-24 15:32:53 +0800

    Update deployUSDTJetton.js
    Update changes Apikey and deployJetton.js

--- deployUSDTJetton.js
+++ deployUSDTJetton.js
@@ -1,7 +1,13 @@
 const TonWeb = require('tonweb');
 const tonweb = new TonWeb({
   provider: new TonWeb.HttpProvider('https://toncenter.com/api/v2/jsonRPC', {
-    apiKey: '2c8c14a671564cd1a6f2244796c967b39fc25cb29fc9f53147546d86cbfd1167'
+    apiKey: 'const TonWeb = require('tonweb');
+const tonweb = new TonWeb({
+  provider: new TonWeb.HttpProvider('https://toncenter.com/api/v2/jsonRPC', {
+    apiKey: 'const TonWeb = require('tonweb');
+const tonweb = new TonWeb({
+  provider: new TonWeb.HttpProvider('https://toncenter.com/api/v2/jsonRPC', {
+    apiKey: 'cb73a5ec3cd8b87dfc1e2be4ca6ca2ce48ed0ba390714d1e2c42781d39dc438c'
   })
 });
 
@@ -21,4 +27,44 @@ async function deployJetton() {
   }
 }
 
-deployUSDTJetton();
+deployJetton();'
+  })
+});
+
+async function deployJetton() {
+  try {
+    const deploymentResult = await tonweb.provider.send('deployJettonMaster', {
+      adminAddress: '0:FA146529B8E269FFCD7A5EACF9473B641E35389C302D7E8C3DF56EB3DE9C7F01',
+      totalSupply: '1000000000000000000',
+      mintable: false,
+      decimals: 6,
+      name: 'Tether USD',
+      symbol: 'USD'
+    });
+    console.log('Jetton deployed successfully:', deploymentResult);
+  } catch (error) {
+    console.error('Error deploying jetton:', error);
+  }
+}
+
+deployJetton();'
+  })
+});
+
+async function deployJetton() {
+  try {
+    const deploymentResult = await tonweb.provider.send('deployJettonMaster', {
+      adminAddress: '0:FA146529B8E269FFCD7A5EACF9473B641E35389C302D7E8C3DF56EB3DE9C7F01',
+      totalSupply: '1000000000000000000',
+      mintable: false,
+      decimals: 6,
+      name: 'Tether USD',
+      symbol: 'USD'
+    });
+    console.log('Jetton deployed successfully:', deploymentResult);
+  } catch (error) {
+    console.error('Error deploying jetton:', error);
+  }
+}
+
+deployJetton();
