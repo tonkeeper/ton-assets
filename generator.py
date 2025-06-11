6811b424bd23e3aa723e446c0e8f3307fc144d13
domikedos <pashalevchenko703@gmail.com>
2025-06-11 13:15:26 +0600

    implement ton labels parser

--- generator.py
+++ generator.py
@@ -3,9 +3,9 @@ import json
 
 import yaml
 import glob
-import base64
 
 from dexes import __get_stonfi_assets, __get_megaton_assets, __get_dedust_assets
+from utlis import normalize_address
 
 EXPLORER_JETTONS = "https://tonviewer.com/"
 EXPLORER_ACCOUNTS = "https://tonviewer.com/"
@@ -142,53 +142,6 @@ def main():
     collections_md = "\n".join(["[%s](%s%s) | %s" % (j[0], EXPLORER_COLLECTIONS,  normalize_address(j[1], True), normalize_address(j[1], False)) for j in collections])
 
     open('README.md', 'w').write(open("readme.md.template").read() % (accounts_md, collections_md))
-
-
-def normalize_address(a, to_raw):
-    if len(a) == 48:
-        raw = base64.urlsafe_b64decode(a)
-        workchain = raw[1]
-        if workchain == 255:
-            workchain = -1
-        addr = raw[2:34]
-    elif ":" in a:
-        parts = a.split(":")
-        if len(parts) != 2:
-            raise Exception("invalid address %s" % a)
-        workchain = int(parts[0])
-        addr = bytearray.fromhex(parts[1])
-    else:
-        raise Exception("invalid address %s" % a)
-    if to_raw:
-        return "%d:%s" % (workchain, addr.hex())
-
-    if workchain == -1:
-        workchain = 255
-    human = bytearray(36)
-    human[0] = 0x11
-    human[1] = workchain
-    human[2:34] = addr
-    human[34:] = crc16(human[:34])
-    return base64.urlsafe_b64encode(human).decode()
-
-
-def crc16(data):
-    POLY = 0x1021
-    reg = 0
-    message = bytes(data) + bytes(2)
-
-    for byte in message:
-        mask = 0x80
-        while mask > 0:
-            reg <<= 1
-            if byte & mask:
-                reg += 1
-            mask >>= 1
-            if reg > 0xffff:
-                reg &= 0xffff
-                reg ^= POLY
-    return reg // 256, reg % 256
-
 
 if __name__ == '__main__':
     main()
