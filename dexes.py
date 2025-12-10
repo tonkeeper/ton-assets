a67c34127fb093c142a4b56712e9534af1d60945
Denis Subbotin <denis@subbot.in>
2025-12-10 13:42:22 +0300

    fix missprint

--- dexes.py
+++ dexes.py
@@ -109,4 +109,4 @@ def update_stonfi_routers():
     if len(routers) == 0:
         return
     with open("accounts/ston.yaml", "w") as f:
-        yaml.safe_dump(routers, f, sort_keys=True, llow_unicode=True)
+        yaml.safe_dump(routers, f, sort_keys=True, allow_unicode=True)
